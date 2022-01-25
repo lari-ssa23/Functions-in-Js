30e1e7f63197ba1aad63cf416013578c97a6fec1
lari-ssa23 <larissa.isabel@unesp.br>
2022-01-24 23:36:59 -0300

    funcoes na linguagem java script

---
+++ script.js
@@ -0,0 +1,31 @@
+ //exercicio 1
+ function frase(){
+    console.log("Black is beautiful")
+    }
+    frase()
+    
+    //exercicio 2
+    function nome(prenom){
+    console.log(`Je mappele ${prenom} `)
+    }
+    nome("Larissa")
+
+//exercicio 3
+    function apresenta(prenom,idade,music){
+    console.log(`Meu nome é ${prenom} tenho ${idade} anos e gosto de ouvir ${music}`)
+    }
+    apresenta("Larissa",22,"Pop")
+    
+    //exercicio 4
+    function entretenimento(movie,song){
+    console.log(`assista ${movie} e escute ${song}`)
+    }
+    entretenimento("Pense Como Eles","All Eyes On Me")
+    
+    //exercicio 5
+    function triplica(x){
+    return 3*x
+    }
+    console.log(triplica(5))
+
+    
